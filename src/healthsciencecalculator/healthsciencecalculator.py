0a39acc2b7dcd54ff4c1e5432f0f8fa904756d3f
Forsythia-olive <forgiveagbesi7478@gmail.com>
2025-01-14 19:20:25 -0800

    Updated implementation

--- src/healthsciencecalculator/healthsciencecalculator.py
+++ src/healthsciencecalculator/healthsciencecalculator.py
@@ -114,12 +114,19 @@ def get_bmr(
     >>> get_bmr(60, 165, 30, "female")
     1392.247
     """
+    if weight <= 0:
+        raise ValueError("Weight must be positive")
+    if height <= 0:
+        raise ValueError("Height must be positive")
+    if age <= 0:
+        raise ValueError("Age must be positive")
+
     if sex.lower() == "male":
         bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
     elif sex.lower() == "female":
         bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
     else:
-        raise ValueError("Invalid value for sex. Use 'male' or 'female'.")
+        raise ValueError('Invalid value for sex. Use "male" or "female".')
     
     return round(bmr, 4)
 
