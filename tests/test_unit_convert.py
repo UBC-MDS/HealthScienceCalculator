4de6891ec3feadc90ba4e7097e0255150a820ab8
interestingtj <49426555+interestingtj@users.noreply.github.com>
2025-01-16 14:09:45 -0800

    test and code for unit convert (#35)
    * write test cases
    
    * write unit convert code and modify unit test
    
    * modify code
    
    return value if input and output are the same
    
    ---------
    
    Co-authored-by: tew49 <tew49@student.ubc.ca>

---
+++ tests/test_unit_convert.py
@@ -0,0 +1,44 @@
+import pytest
+from healthsciencecalculator.healthsciencecalculator import unit_convert
+
+# Test case 1: wrong input type
+
+def test_valid_input():
+    
+    with pytest.raises(TypeError):
+        unit_convert("a","kg","g")
+
+    with pytest.raises(TypeError):
+        unit_convert(10,20,30)
+
+# Test case 2: input and output not belong to the same category
+
+def test_category():
+
+    with pytest.raises(ValueError):
+        unit_convert(10,"g","m")
+
+    with pytest.raises(ValueError):
+        unit_convert(20,"C","stone")
+
+# Test case 3: input or output not exist
+
+def test_quantites():
+
+    with pytest.raises(ValueError):
+        unit_convert(10,"g","mg")
+
+    with pytest.raises(ValueError):
+        unit_convert(10,"cm","mm")
+
+# Test case 4: success
+
+def test_success():
+
+    assert unit_convert(1,"kg","g") == 1000
+
+    assert unit_convert(1,"m","cm") == 100
+
+    assert unit_convert(1,"L","mL") == 1000
+
+    assert unit_convert(10,"C","F") == 50
