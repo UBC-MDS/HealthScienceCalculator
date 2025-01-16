4b0f4f9f0428448346d1b59f638d5dd540b79ac2
interestingtj <49426555+interestingtj@users.noreply.github.com>
2025-01-16 14:48:44 -0800

    add more test cases and doc
    unit_convert

--- tests/test_unit_convert.py
+++ tests/test_unit_convert.py
@@ -4,6 +4,10 @@ from healthsciencecalculator.healthsciencecalculator import unit_convert
 # Test case 1: wrong input type
 
 def test_valid_input():
+    """
+    Test whether unit_convert raises TypeError 
+    when wrong datatypes are input
+    """
     
     with pytest.raises(TypeError):
         unit_convert("a","kg","g")
@@ -11,10 +15,16 @@ def test_valid_input():
     with pytest.raises(TypeError):
         unit_convert(10,20,30)
 
+    with pytest.raises(TypeError):
+        unit_convert(10,"kg",30)
+
 # Test case 2: input and output not belong to the same category
 
 def test_category():
-
+    """
+    Test whether unit_convert raises ValueError 
+    when two unrelated units are input
+    """
     with pytest.raises(ValueError):
         unit_convert(10,"g","m")
 
@@ -23,22 +33,34 @@ def test_category():
 
 # Test case 3: input or output not exist
 
-def test_quantites():
-
+def test_units():
+    """
+    Test whether unit_convert raises ValueError
+    when unit does not exist
+    """
     with pytest.raises(ValueError):
         unit_convert(10,"g","mg")
 
     with pytest.raises(ValueError):
         unit_convert(10,"cm","mm")
 
+    with pytest.raises(ValueError):
+        unit_convert(20,"aaa","stone")
+
 # Test case 4: success
 
 def test_success():
-
+    """
+    Test whether unit_convert return right answers
+    """
     assert unit_convert(1,"kg","g") == 1000
+
+    assert unit_convert(1,"kg","kg") == 1
 
     assert unit_convert(1,"m","cm") == 100
 
     assert unit_convert(1,"L","mL") == 1000
 
     assert unit_convert(10,"C","F") == 50
+
+    assert unit_convert(50,"F","C") == 10
